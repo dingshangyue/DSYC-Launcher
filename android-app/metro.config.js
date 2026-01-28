47750e8a0f672fed6d89eccd2e419cb2a82f5ff1
mac <mac@macdeMacBook-Air.local>
2026-01-28 18:09:00 +0800

    Fix JavaScript bundling error and bump version to 1.0.19

--- android-app/metro.config.js
+++ android-app/metro.config.js
@@ -2,4 +2,7 @@ const { getDefaultConfig } = require('expo/metro-config');
 
 const config = getDefaultConfig(__dirname);
 
+// Add custom configurations if needed
+config.resolver.sourceExts = [...config.resolver.sourceExts, 'ts', 'tsx'];
+
 module.exports = config;
